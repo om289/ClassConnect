73a0cff2cf6510735779b6ab5b44c9d9b2dc8551
om289 <ommetha37@gmail.com>
2025-04-05 14:20:00 +0530

    refactor(db): add comments and clean database connection configuration

--- database.php
+++ database.php
@@ -1,9 +1,12 @@
-<?php
+﻿<?php
 
+// Database connection configuration
 $server = "localhost";
 $user = "root";
 $passdb = "";
 $db = "cc_db";
+
+// Connect to the CC database or terminate with error
 $connect = mysqli_connect( $server, $user, $passdb, $db )or die( "Connection Error" );
 
 ?>
